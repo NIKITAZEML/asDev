f5292ed82773655dae0c161d2b97bc06f312a724
erohin-danila <erohindani@yandex.ru>
2022-05-27 17:20:48 +0300

    bl

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
   {
     path: '/about',
     name: 'about',
