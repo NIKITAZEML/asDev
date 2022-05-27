145a4f15f8b0dae460f0a10ee483547f1cdf9bfd
erohin-danila <erohindani@yandex.ru>
2022-05-27 13:33:18 +0300

    startHeader

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import styles from './assets/styles/main.scss';
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  styles,
   render: h => h(App)
 }).$mount('#app')
