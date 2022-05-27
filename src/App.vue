e02a387c3a92cac7659969679787e54ee5346b17
erohin-danila <erohindani@yandex.ru>
2022-05-27 18:54:12 +0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <AppHeader/>
     <router-view/>
+    <AppFooter/>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
-
+import AppFooter from '@/components/AppFooter.vue'
 export default {
  
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
-import HomeView from '../views/HomeView.vue'
-=======
 import HomeView from '../views/HomeView'
->>>>>>> f1633970378b3d62489f41d85e8b8c36ac01b046
 
 Vue.use(VueRouter)
 
 const routes = [
-<<<<<<< HEAD
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-=======
->>>>>>> f1633970378b3d62489f41d85e8b8c36ac01b046
   {
     path: '/',
     name: 'HomeView',
