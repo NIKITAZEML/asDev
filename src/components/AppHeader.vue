bd11aa5fda196c135fbae0a51d282c416b31b57c
Dmitriy_Kovalenko <imfreedomyt@gmail.com>
2022-05-28 17:49:24 +0300

    fixbags

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,14 +13,14 @@
                         </transition>
                    </div>
                          <div v-bind:class="{nav_open:openOb}"  class="nav">
-                                <div class="nav-block-value">
-                                <router-link :to="'/tragde'" class="header__nav">Обмен валют</router-link>
-                                <router-link :to="'/'" class="header__nav">Курс валют</router-link>
-                                </div>
-                                <div class="nav-block-singin">
-                                <router-link :to="'/auth'"><div class="header-login">Войти</div></router-link>
-                                <router-link :to="'/auth'"><div class="header-register">Зарегистрироваться</div></router-link>
-                                </div>
+                            <div class="nav-block-value">
+                                <router-link :to="'/trade'" class="header__nav">Обмен валют</router-link>
+                                <router-link :to="'/cource'" class="header__nav">Курс валют</router-link>
+                            </div>
+                            <div class="nav-block-singin">
+                                <router-link :to="'/auth'" class="header-login">Войти</router-link>
+                                <router-link :to="'/auth'" class="header-register">Зарегистрироваться</router-link>
+                            </div>
                          </div>
                         </div>
                 </nav>
@@ -52,12 +52,15 @@ export default{
 
 <style scoped lang="scss">
     @import "src/assets/styles/fonts";
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .9s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
-  opacity: 0;
-}
+
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .9s;
+    }
+
+    .fade-enter, .fade-leave-to {
+      opacity: 0;
+    }
+
     .container{
         width: size(1440, 1920);
         margin: 0 auto;
@@ -89,12 +92,14 @@ export default{
         width: 100%;
      
     }
+
     .nav{
         width: 60%;
         align-items: center;
         justify-content: space-between;
         display: flex;
     }
+
     .nav-block {
     display: flex;
     justify-content: space-between;
@@ -102,7 +107,8 @@ export default{
     text-align: center;
     width: size(1440, 1920);
     margin: 0 auto;
-}
+    }
+
     .header__nav{
         margin-left: size(24, 1920);
         font-style: normal;
@@ -112,10 +118,8 @@ export default{
         color: #FFFFFF;
     }
 
-
     .header-login{
         border: 1px solid #BCD0E5;
-        font-family: 'Raleway';
         padding: size(12, 1920) size(28, 1920);
         font-weight: 400;
         font-size: size(16, 1920);
@@ -133,29 +137,31 @@ export default{
         text-transform: uppercase;
         transition: 0.2s ease-out;
     }
+
     // анимации
+
   .header-login:hover {
     background-color: #6d44b8;
     box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    color: rgb(0, 0, 0);
     -moz-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     -webkit-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     font-weight: 800;
     color: white;
-     transform: scale(1.2);
-     border: 0;
-     border-radius: 2px;
+    transform: scale(1.2);
+    border: 0;
+    border-radius: 2px;
     }
-     .header-register:hover {
+
+    .header-register:hover {
     background-color: #6d44b8;
     box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    color: rgb(0, 0, 0);
     -moz-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     -webkit-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     font-weight: 800;
     transform: scale(1.1);
-     color: white;
+    color: white;
     }
+
      .nav-block-singin{
          display: flex;
          justify-content: space-between;
@@ -182,51 +188,56 @@ export default{
     text-transform: uppercase;
     transition: 0.2s ease-out;
     }
+
     .burger-menu{
         display: none;
     }
+
     .nav-block-logo{
         width: 20%;
     }
+
     @media (max-width: 750px){
-       
-         .burger-menu{
+
+    .burger-menu{
     display: block;
     width: size(40, 750);
     height: size(40, 750);
     position: absolute;
     top: size(20, 750);
     right: size(40, 750);
-}
-    .nav{  
-        top:-100%;
-         transition: 0.5s ease-out;
-        left: 0;
-        margin: 0;
-        flex-direction: column;
-        z-index: 9;
-        position: absolute;
-        height: 100%;
-        background-color: rgb(61, 14, 75);
-        width: 100%;
-         padding: size(25, 750);
-         display: flex;
-         flex-direction: column;
-         justify-content: center;
     }
+
+    .nav{  
+    top:-100%;
+    transition: 0.5s ease-out;
+    left: 0;
+    margin: 0;
+    flex-direction: column;
+    z-index: 9;
+    position: absolute;
+    height: 100%;
+    background-color: rgb(61, 14, 75);
+    width: 100%;
+    padding: size(25, 750);
+    display: flex;
+    justify-content: center;
+    }
+
     .nav_open{
          top:size(80, 750);
     }
+
       .header__nav{
         font-style: normal;
         font-weight: 400;
         font-size: size(60, 750);
-        line-height: size(28,
-         1920);
+        line-height: size(28, 1920);
         color: #FFFFFF;
         margin: 0;
         margin-bottom: size(60, 750);
     }
+
       .header{
         display: flex;
         justify-content: space-between;
@@ -235,10 +246,10 @@ export default{
         height: size(80, 750);
         z-index: 999;
     }
+
     .header-login{
         margin: 0;
         border: 1px solid #BCD0E5;
-        font-family: 'Raleway';
         padding: size(12, 750) size(28, 750);
         font-weight: 400;
         font-size: size(20, 750);
@@ -246,51 +257,57 @@ export default{
         color: #FFFFFF;
         width: 100%;
         height: size(60, 750);
-         vertical-align: center;
+        vertical-align: center;
         text-align: center;
         margin-bottom: size(30, 750);
     }
+
     .header-register{
-          font-size: size(20, 750);
-           padding: size(12, 750) size(28, 750);
-           line-height: size(19, 750);
-           width: 100%;
-           height: size(60, 750);
-           vertical-align: center;
-           text-align: center;
-           margin-left: 0;
+        font-size: size(20, 750);
+        padding: size(12, 750) size(28, 750);
+        line-height: size(19, 750);
+        width: 100%;
+        height: size(60, 750);
+        vertical-align: center;
+        text-align: center;
+        margin-left: 0;
     }
+
     a{
         width: 100%;
     }
+
     .nav-block{
-     display: flex;
+        display: flex;
         justify-content: space-between;
         align-items: center;
         text-align: center;
         vertical-align: center;
         height: 30%;
-}
- .header-logo{
-        width: size(150, 750);
-        height: size(20, 750);
     }
-       .container{
-        width: size(700, 750);
-        margin: 0 auto;
+
+    .header-logo{
+    width: size(150, 750);
+    height: size(20, 750);
     }
+
+    .container{
+    width: size(700, 750);
+    margin: 0 auto;
+    }
+
     .nav-block-value{
-         display: flex;
-      
-         width: 80%;
-         flex-direction: column;
+    display: flex;
+    width: 80%;
+    flex-direction: column;
     }
-       .nav-block-singin{
-         display: flex;
-         justify-content: space-between;
-         align-items: center;
-         width: 80%;
-         flex-direction: column;
-       }
+
+    .nav-block-singin{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    flex-direction: column;
     }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,28 +14,36 @@
                 <i></i>
               </span>
          </div>
-          <router-link for="chk"  :to="'/auth'"><div class="main-button__register">Зарегистрироваться </div></router-link>
+          <router-link for="chk"  :to="'/auth'" class="main-button__register">Зарегистрироваться</router-link>
         </div>
       </div>
     </div>
   </section>
   <section class="popularcrypto">
     <div class="container">
+
+      <div class="crypto-title">
+        <span class="crypto-title__text">Популярные криптовалюты</span>
+          <div class="crypto-checkall">
+            <span class="crypto-checkall__text">Посмотреть все</span>
+            <img src="../assets/images/icons/checkall.png">
+          </div>
+      </div>
       <div class="crypto-cards">
         <div class="crypto-cards__cardsell">
-        <input type="number" placeholder="Потратите">
+          <input type="number" placeholder="Потратите">
           <select>
             <option>1</option>
           </select>
         </div>
         <div class="crypto-cards__cardreceive">
-        <input type="number" placeholder="Получите">
+          <input type="number" placeholder="Получите">
           <select>
             <option>2</option>
           </select>
         </div>
         <div class="crypto-cards__cardreg">
-        <input type="number" placeholder="Получите при регистрации">
+          <input type="number" placeholder="Получите при регистрации">
           <select>
             <option>3</option>
           </select>
@@ -43,13 +51,6 @@
         <div class="crypto-cards__button">
           Рассчитать
         </div>
-      </div>
-      <div class="crypto-title">
-        <span class="crypto-title__text">Популярные криптовалюты</span>
-          <div class="crypto-checkall">
-            <span class="crypto-checkall__text">Посмотреть все</span>
-            <img src="../assets/images/icons/checkall.png">
-          </div>
       </div>
       <div class="crypto-stats">
         <span class="crypto-stats__text">Название </span>
@@ -70,7 +71,7 @@
       </div>
       <div class="crypto-register__title">
         <span class="crypto-title__text">Зарегистрируйтесь сейчас и получите более выгодный курс (5%)</span>
-        <div class="register-title__button">Начать</div>
+        <router-link :to="'/auth'" class="register-title__button">Начать</router-link>
       </div>
     </div>
   </section>
@@ -146,31 +147,32 @@ export default {
       color: #8A8A8A;
     }
       .main-button__email:hover{
-        -webkit-box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
-      -moz-box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
-      box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);}
+        -webkit-box-shadow: 4px 4px 8px 0 rgba(34, 60, 80, 0.2);
+        -moz-box-shadow: 4px 4px 8px 0 rgba(34, 60, 80, 0.2);
+        box-shadow: 4px 4px 8px 0 rgba(34, 60, 80, 0.2);}
   }
-.main-button__email ~ .focus-border:before,
-.main-button__email ~ .focus-border:after{content: ""; position: absolute; top: 0; right: 0; width: 0; height: 3px; background-color: #6d44b8; transition: 0.2s; transition-delay: 0.2s;}
-.main-button__email ~ .focus-border:after{top: auto; bottom: 0; right: auto; left: 0; transition-delay: 0.6s;}
-.main-button__email ~ .focus-border i:before,
-.main-button__email ~ .focus-border i:after{content: ""; position: absolute; top: 0; left: 0; width: 3px; height: 0; background-color: #6d44b8; transition: 0.2s;}
-.main-button__email ~ .focus-border i:after{left: auto; right: 0; top: auto; bottom: 0; transition-delay: 0.4s;}
-.main-button__email:focus ~ .focus-border:before,
-.main-button__email:focus ~ .focus-border:after{width: 100%; transition: 0.2s; transition-delay: 0.6s; }
-.main-button__email:focus ~ .focus-border:after{transition-delay: 0.2s;}
-.main-button__email:focus ~ .focus-border i:before,
-.main-button__email:focus ~ .focus-border i:after{height: 100%; transition: 0.2s;}
-.main-button__email:focus ~ .focus-border i:after{transition-delay: 0.4s;}
-:focus{outline: none;}
-.main-button__email:focus{
--webkit-box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);
--moz-box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);
-box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);}
-/* necessary to give position: relative to parent. */
-input[type="text"]{font: 15px/24px 'Muli', sans-serif; color: #333; width: 100%; box-sizing: border-box; letter-spacing: 1px;}
-.col-3{ position: relative;}
-:focus{outline: none;}
+
+  .main-button__email ~ .focus-border:before,
+  .main-button__email ~ .focus-border:after{content: ""; position: absolute; top: 0; right: 0; width: 0; height: 3px; background-color: #6d44b8; transition: 0.2s; transition-delay: 0.2s;}
+  .main-button__email ~ .focus-border:after{top: auto; bottom: 0; right: auto; left: 0; transition-delay: 0.6s;}
+  .main-button__email ~ .focus-border i:before,
+  .main-button__email ~ .focus-border i:after{content: ""; position: absolute; top: 0; left: 0; width: 3px; height: 0; background-color: #6d44b8; transition: 0.2s;}
+  .main-button__email ~ .focus-border i:after{left: auto; right: 0; top: auto; bottom: 0; transition-delay: 0.4s;}
+  .main-button__email:focus ~ .focus-border:before,
+  .main-button__email:focus ~ .focus-border:after{width: 100%; transition: 0.2s; transition-delay: 0.6s; }
+  .main-button__email:focus ~ .focus-border:after{transition-delay: 0.2s;}
+  .main-button__email:focus ~ .focus-border i:before,
+  .main-button__email:focus ~ .focus-border i:after{height: 100%; transition: 0.2s;}
+  .main-button__email:focus ~ .focus-border i:after{transition-delay: 0.4s;}
+  :focus{outline: none;}
+  .main-button__email:focus{
+  -webkit-box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);
+  -moz-box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);
+  box-shadow: 4px 4px 44px -14px rgba(26, 0, 255, 0.68);}
+  input[type="text"]{font: 15px/24px 'Muli', sans-serif; color: #333; width: 100%; box-sizing: border-box; letter-spacing: 1px;}
+  .col-3{ position: relative;}
+  :focus{outline: none;}
+
   .main-button__register{
     background: #FFFFFF;
     text-align: center;
@@ -179,12 +181,6 @@ input[type="text"]{font: 15px/24px 'Muli', sans-serif; color: #333; width: 100%;
     line-height: size(19, 1920);
     color: #000000;
     padding: size(12, 1920) size(17, 1920);
-    -moz-border-radius: 2px;
-    -moz-transition: 0.2s ease-out;
-    -ms-transition: 0.2s ease-out;
-    -o-transition: 0.2s ease-out;
-    -webkit-border-radius: 2px;
-    -webkit-transition: 0.2s ease-out;
     background-clip: padding-box;
     display: inline-block;
     text-decoration: none;
@@ -192,18 +188,17 @@ input[type="text"]{font: 15px/24px 'Muli', sans-serif; color: #333; width: 100%;
     transition: 0.2s ease-out;
   }
   .main-button__register:hover {
-   
-      transform: scale(1.2);
+    transform: scale(1.2);
     background-color: #6d44b8;
     box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    color: rgb(0, 0, 0);
     -moz-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     -webkit-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     font-weight: 800;
-     color: white;
-     text-align: center;
-     vertical-align: center;
+    color: white;
+    text-align: center;
+    vertical-align: center;
     }
+
   /*------------TEXT-STYLE---------------*/
 
   .main__title{
@@ -286,28 +281,23 @@ input[type="text"]{font: 15px/24px 'Muli', sans-serif; color: #333; width: 100%;
     font-size: size(16, 1920);
     line-height: size(19, 1920);
     color: black;
-     transition: 0.2s ease-out;
-     cursor: pointer;
-     border-radius: 5px;
-     background: #49DEDA;
-  box-shadow: inset .125em .125em .5em hsl(178.39,69.3%,57.84%), inset -.125em -.125em .5em hsl(178.39,69.3%,57.84%);
- transition: all .5s linear 0s;
+    cursor: pointer;
+    border-radius: 5px;
+    background: #49DEDA;
+    box-shadow: inset .125em .125em .5em hsl(178.39,69.3%,57.84%), inset -.125em -.125em .5em hsl(178.39,69.3%,57.84%);
+    transition: all .5s linear 0s;
   }
 .crypto-cards__button:hover{
-  color: white;
-  -webkit-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
--moz-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
- cursor: pointer;
- color: white;
-   transition: all 1s ease-out;
- background: rgb(60, 52, 172);
-  box-shadow: inset .125em .125em .5em hsl(251.61,64.58%,18.82%), inset -.125em -.125em .5em hsl(251.61,64.58%,18.82%), ;
+    color: white;
+    cursor: pointer;
+    transition: all 1s ease-out;
+    background: rgb(60, 52, 172);
+    box-shadow: inset .125em .125em .5em hsl(251.61,64.58%,18.82%), inset -.125em -.125em .5em hsl(251.61,64.58%,18.82%), ;
 }
   /*------Crypto----Popylaryty------*/
 
   .crypto-title{
-    margin-top: size(100, 1920);
+    margin-bottom: size(50, 1920);
     display: flex;
     justify-content: space-between;
   }
@@ -367,12 +357,15 @@ box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
     align-items: center;
     margin-top: size(30, 1920);
   }
+
   .stats-card__price{
     width: size(420, 1920);
   }
+
   .stats-card__timeprice{
     width: size(485, 1920);
   }
+
   .stats-card__name, .stats-card__price, .stats-card__timeprice, .stats-card__largeprice{
     font-weight: 400;
     font-size: size(20, 1920);
@@ -398,29 +391,23 @@ box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
     display: flex;
     align-items: center;
     justify-content: center;
-    background: #46DFDD;
     font-weight: 600;
     font-size: size(16, 1920);
     line-height: size(19, 1920);
     color: black;
-     transition: all 1s ease-out;
-     cursor: pointer;
-     border-radius: 5px;
- background: #49DEDA;
-  box-shadow: inset .125em .125em .5em hsl(178.39,69.3%,57.84%), inset -.125em -.125em .5em hsl(178.39,69.3%,57.84%);
- transition: all .5s linear 0s;
+    cursor: pointer;
+    border-radius: 5px;
+    background: #49DEDA;
+    box-shadow: inset .125em .125em .5em hsl(178.39,69.3%,57.84%), inset -.125em -.125em .5em hsl(178.39,69.3%,57.84%);
+    transition: all .5s linear 0s;
   }
   
   .register-title__button:hover{
-     background-color: #6d44b8;
-  color: white;
-  -webkit-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
--moz-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
-box-shadow: 4px 4px 44px 0px rgba(59, 40, 225, 0.65);
-  transition: all 1s ease-out;
- background: rgb(60, 52, 172);
-  box-shadow: inset .125em .125em .5em hsl(251.61,64.58%,18.82%), inset -.125em -.125em .5em hsl(251.61,64.58%,18.82%), ;
- cursor: pointer;
+    color: white;
+    transition: all 1s ease-out;
+    background: rgb(60, 52, 172);
+    box-shadow: inset .125em .125em .5em hsl(251.61,64.58%,18.82%), inset -.125em -.125em .5em hsl(251.61,64.58%,18.82%), ;
+    cursor: pointer;
   }
 
 </style>
