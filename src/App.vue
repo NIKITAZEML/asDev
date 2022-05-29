1ca9c4cf3bb0887fe045f3c864e7fc30d2ad014a
Илья Богданчиков <test200373@gmail.com>
2022-05-29 13:33:03 +0300

    history & my cins

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <AppHeader/>
+    <component :is="getLayout"></component>
+<!--    <AppHeader/>-->
     <router-view/>
     <AppFooter/>
   </div>
@@ -10,7 +11,11 @@
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
 export default {
- 
+  computed: {
+    getLayout() {
+      return this.$route.meta.layout
+    }
+  },
   components: {
     AppHeader,
     AppFooter
